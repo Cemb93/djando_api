b2097ef18c05712bc9e7afb7135401c1654c875b
Cemb93 <karloz13m@gmail.com>
2025-04-08 13:24:22 -0500

    Validacion de datos

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -12,13 +12,21 @@ class MenuItemSerializer(serializers.ModelSerializer):
   stock = serializers.IntegerField(source='inventory')
   price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
   category = CategorySerializer(read_only=True)
+  category_id = serializers.IntegerField(write_only=True)# ? SE USA PARA EL POST EN LA RELACION
   class Meta:
     model = MenuItem
-    fields = ['id','title', 'price', 'stock', 'price_after_tax', 'category']
-    extra_kwargs = {
-      'price': {'min_value': 2},
-      'inventory':{'min_value':0}
-    }
+    fields = ['id','title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
+    # extra_kwargs = {
+    #   'price': {'min_value': 2},
+    #   'inventory':{'min_value':0}
+    # }
+  
+  def validate(self, attrs):
+    if attrs['price'] < 2:
+      raise serializers.ValidationError('Price should not be less than 2.0')
+    if attrs['inventory'] < 0:
+      raise serializers.ValidationError('Stock cannot be negative')
+    return super().validate(attrs)
   
   def calculate_tax(self, product: MenuItem):
     return product.price * Decimal(1.1)
